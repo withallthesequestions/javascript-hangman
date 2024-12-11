4aa9b905f41a444db79d5646cdcc4d1bfaaaac91
withallthesequestions <uytpic434516@gmail.com>
2024-12-10 23:08:42 -0500

    Up

--- script.js
+++ script.js
@@ -1,68 +1,144 @@
-const ALPHABETS = 'abcdefghijklmnopqrstuvwxyz';
-const TRY_CONSTANT = 12;
-const CHALLENGE_STRING = 'pythagorean theorem';
-const challengeStringChars = CHALLENGE_STRING.split('');
+const TRIES_LEFT = 12;
+const CHALLENGE_STRING = 'party pooper';
+const ALPHABETS = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+];
 
-const blanksDisplay = document.getElementById('blanksDisplay');
-const tryCountDisplay = document.getElementById('numberOfTries');
-const lettersGuessedDisplay = document.getElementById('lettersGuessed');
-const keyboardDisplay = document.getElementById('keyboard');
-const hangmanDisplay = document.getElementById('hangmanImage');
+const blanksDisplay = document.querySelector('#blanksDisplay');
+const triesLeftDisplay = document.querySelector('#numberOfTries');
+const lettersGuessedDisplay = document.querySelector('#lettersGuessed');
+const keyboardDisplay = document.querySelector('#keyboard');
+const hangmanDisplay = document.querySelector('#hangmanImage');
 
-initialDisplay();
-function initialDisplay() {
-	blanksDisplay.innerText = challengeStringChars.map((x) =>
-		x === ' ' ? ' ' : '_'
-	);
-	tryCountDisplay.innerText = TRY_CONSTANT;
-	generateKeyboard(ALPHABETS);
-	hangmanDisplay.src = './images/hangman-0.svg';
-}
-function generateKeyboard(letters) {
+const challengeArray = CHALLENGE_STRING.split('');
+/* 
+ - It's tempting to incorporate the above array into the below `generateBlanks` function as a split-map-join operation. This would helps overall code readability. 
+ - But a later function (`updateBlanksOnUserGuess`), also makes use of this array.
+*/
+
+const validateChallengeString = (string) => {
+	const regex = /[^a-z\s]/gi;
+	const invalidCharacters = string.match(regex);
+	if (!!invalidCharacters) {
+		for (const button of keyboardDisplay.children) {
+			button.setAttribute('disabled', '');
+		}
+		return 'Error: Invalid character in challenge string';
+	} else {
+		blanksDisplay.innerText = generateBlanks(challengeArray);
+	}
+};
+
+const generateBlanks = (arrayOfLetters) => {
+	if (validateChallengeString(CHALLENGE_STRING)) {
+		return validateChallengeString(CHALLENGE_STRING);
+	} else {
+		const blanksString = arrayOfLetters
+			.map((letter) => (letter === ' ' ? `\xa0` : '_'))
+			.join(' ');
+		return blanksString;
+	}
+};
+
+const renderScreenKeyboard = (letters) => {
 	for (char of letters) {
-		const keyboardButton = document.createElement('button');
-		keyboardButton.textContent = char;
-		keyboardDisplay.appendChild(keyboardButton);
-		keyboardButton.addEventListener('click', (e) => {
+		const kbdBtn = document.createElement('button');
+		kbdBtn.textContent = char;
+		keyboardDisplay.appendChild(kbdBtn);
+		kbdBtn.addEventListener('click', (e) => {
 			handleUserGuess(e.target.innerText);
 		});
 	}
-}
-function handleUserGuess(key) {
-	if (!CHALLENGE_STRING.includes(key)) {
-		tryCountDisplay.innerText--;
-		updateHangmanImage(tryCountDisplay.innerText);
-		checkForDefeat(tryCountDisplay.innerText);
-	}
-	lettersGuessedDisplay.innerText += key;
-	disableKey(key);
-	blanksDisplay.innerText = challengeStringChars.map((x) =>
-		x === ' ' ? ' ' : lettersGuessedDisplay.innerText.includes(x) ? x : '_'
-	);
-	checkForVictory();
-}
-function updateHangmanImage(numberOfTries) {
-	const hangNumber = Math.floor((TRY_CONSTANT - numberOfTries) / 2);
-	// -0 to -6
-	hangmanDisplay.src = `./images/hangman-${hangNumber}.svg`;
-}
-function disableKey(letter) {
-	for (const button of keyboardDisplay.children) {
-		if (button.innerText === letter) {
-			button.setAttribute('disabled', '');
-		}
-	}
-}
-function checkForDefeat(numberOfTries) {
+};
+
+const receiveUserKeyInputs = () => {
+	addEventListener('keydown', (e) => {
+		if (!ALPHABETS.includes(e.key)) {
+			console.log('You pressed an invalid key!');
+		} else handleUserGuess(e.key);
+	});
+};
+
+const renderStartScreen = () => {
+	triesLeftDisplay.innerText = TRIES_LEFT;
+	hangmanDisplay.src = './images/hangman-0.svg';
+	renderScreenKeyboard(ALPHABETS);
+	receiveUserKeyInputs();
+};
+
+renderStartScreen();
+
+const checkDefeatCondition = (numberOfTries) => {
 	if (numberOfTries <= 0) {
-		console.log('You lost!');
+		console.log('You lost');
 		for (const button of keyboardDisplay.children) {
 			button.setAttribute('disabled', '');
 		}
 	}
-}
-function checkForVictory() {
+};
+
+const updateHangmanImage = (numberOfTries) => {
+	const hangNumber = Math.floor((TRIES_LEFT - numberOfTries) / 2);
+	// -0 to -6
+	hangmanDisplay.src = `./images/hangman-${hangNumber}.svg`;
+};
+
+const disableKey = (letter) => {
+	const keyIndex = ALPHABETS.indexOf(letter);
+	keyboardDisplay.children[keyIndex].setAttribute('disabled', '');
+};
+
+const updateBlanksOnUserGuess = () => {
+	blanksDisplay.innerText = challengeArray
+		.map((x) =>
+			x === ' '
+				? `\xa0`
+				: lettersGuessedDisplay.innerText.includes(x)
+				? x
+				: '_'
+		)
+		.join(' ');
+};
+
+const checkVictoryCondition = () => {
 	if (!blanksDisplay.innerText.includes('_')) {
 		console.log('You won!');
 	}
-}
+};
+
+const handleUserGuess = (key) => {
+	if (!CHALLENGE_STRING.includes(key)) {
+		triesLeftDisplay.innerText--;
+		checkDefeatCondition(triesLeftDisplay.innerText);
+		updateHangmanImage(triesLeftDisplay.innerText);
+	}
+	lettersGuessedDisplay.innerText += key;
+	disableKey(key);
+	updateBlanksOnUserGuess();
+	checkVictoryCondition();
+};
